ec8d683046851b492758087fbbb45fbad2906002
Chong Ren <leechongren@users.noreply.github.com>
2020-01-28 16:15:14 +0800

    initialised monster and player objects

---
+++ src/objects/monster.js
@@ -0,0 +1,26 @@
+const MONSTER = [
+    {
+        "id": 1,
+        "type": "Goblin",
+        "base": {
+            "HP": 15,
+            "Attack": 2,
+            "Defence": 2,
+        },
+        "exp": 5
+    },
+
+    {
+        "id": 2,
+        "type": "Orc",
+        "base": {
+            "HP": 50,
+            "Attack": 2,
+            "Defence": 2,
+        },
+        "exp": 10
+    }
+
+]
+
+export default MONSTER
---
+++ src/objects/player.js
@@ -0,0 +1,24 @@
+const PLAYER = [
+    {
+        "id": 1,
+        "type": "Warrior",
+        "base": {
+            "HP": 50,
+            "Attack": 3,
+            "Defence": 2,
+        }
+    },
+
+    {
+        "id": 2,
+        "type": "Rogue",
+        "base": {
+            "HP": 30,
+            "Attack": 2,
+            "Defence": 2,
+        }
+    }
+
+]
+
+export default PLAYER
