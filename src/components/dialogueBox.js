15bd2fe84037e523b58ca44f532f0de3d4b2df87
Chong Ren <leechongren@users.noreply.github.com>
2020-01-30 01:01:58 +0800

    added defeated by monster dialogue box

--- src/components/dialogueBox.js
+++ src/components/dialogueBox.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './dialogueBox.css'
-
+import { Link } from 'react-router-dom'
 
 export const DialogueBox = ({ playerAttack, monster, monsterAttack }) => {
     return <div className="dialogue-box">
@@ -14,6 +14,18 @@ export const DefeatMonsterDialogueBox = ({ monster, exp }) => {
         <p className="text-paragraph"> You defeated {monster}!<br />
             You gained {exp} experience points!
         </p>
+    </div>
+}
+
+export const DefeatedDialogueBox = () => {
+    return <div className="dialogue-box">
+        <p className="text-paragraph"> You have been defeated...<br />
+            Please try again
+        </p><br />
+        <Link to='/'>
+            <button>Return to home</button>
+        </Link>
+
     </div>
 }
 
