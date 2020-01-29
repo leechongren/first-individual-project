ec42cc49863806401cb04f71dfffb954d47326c7
Chong Ren <leechongren@users.noreply.github.com>
2020-01-30 01:04:30 +0800

    added hp stat

--- src/containers/characterDisplay.js
+++ src/containers/characterDisplay.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import playerdata from '../objects/player'
 import { Link } from 'react-router-dom'
-// 1. change to class component
-// 2. create state for profile
-// 3. Use Route to show profile, pass data from state to profile props
-// 3. use route to show battle, pass method to update states
+
 class CharacterDisplay extends React.Component {
     constructor(props) {
         super(props)
@@ -12,14 +9,16 @@ class CharacterDisplay extends React.Component {
             Vocation: playerdata[0].type,
             HP: playerdata[0].base.HP,
             Attack: playerdata[0].base.Attack,
-            Defence: playerdata[0].base.Defence
+            Defence: playerdata[0].base.Defence,
+            Acquired_exp: playerdata[0].acquired_exp
         }
     }
 
     render() {
         const originalStats = {
             pathname: "/battle",
-            HP: this.state.HP
+            HP: this.state.HP,
+            Acquired_exp: this.state.Acquired_exp
         }
         return <div>
             Character Profile <br />
@@ -27,6 +26,7 @@ class CharacterDisplay extends React.Component {
             HP: {this.state.HP} <br />
             Attack: {this.state.Attack} <br />
             Defence: {this.state.Defence} <br />
+            Acquired Exp: {this.state.Acquired_exp} <br />
             <Link to={originalStats}>
                 <button>To Battle!</button>
             </Link>
