dd716c88f450f6bd9d68c4b1ef3e73029492073a
Chong Ren <leechongren@users.noreply.github.com>
2020-01-31 14:17:03 +0800

    added gif in homepage and css for hover

--- src/containers/homePage.js
+++ src/containers/homePage.js
@@ -4,7 +4,10 @@ import './homePage.css'
 
 const HomePage = () => {
     return <div className='home-page-div'>
-        Welcome to a simple RPG game! <br />
+        <div >
+            <img className='gif-intro-page' alt="gif" src="https://media2.giphy.com/media/B2eg9CmW4i9ZC/giphy.gif?cid=790b761164642416952e19e6d05a7aae9228d4c4cd191bc6&rid=giphy.gif" />
+        </div>
+        <div className="intro-text">Welcome to a simple RPG game!</div> <br />
         <Link className='start-game-link' to='/profile'>
             <div className='start-game-icon'>
                 Start Game
