6a4edeea56a9408bfed12eb5eebe1e2f909dc69a
leechongren <lee_chongren@hotmail.com>
2020-03-30 13:52:00 +0800

    add Register page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import BattleDisplay from './containers/BattleDisplay'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import HomePage from './containers/HomePage'
-import CharacterDisplay from './containers/CharacterDisplay'
-import CharacterInBattleDisplay from './containers/CharacterInBattleDisplay';
-
-
-
-
+import React from "react";
+import BattleDisplay from "./containers/BattleDisplay";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./containers/HomePage";
+import CharacterDisplay from "./containers/CharacterDisplay";
+import CharacterInBattleDisplay from "./containers/CharacterInBattleDisplay";
+import RegisterPage from "./containers/RegisterPage";
 
 function App() {
   return (
@@ -15,10 +12,15 @@ function App() {
       <div className="App">
         <div>
           <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/profile' component={CharacterDisplay} />
-            <Route exact path='/battle' component={BattleDisplay} />
-            <Route exact path='/battle/profile' component={CharacterInBattleDisplay} />
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/profile" component={CharacterDisplay} />
+            <Route exact path="/battle" component={BattleDisplay} />
+            <Route
+              exact
+              path="/battle/profile"
+              component={CharacterInBattleDisplay}
+            />
+            <Route exact path="/register" component={RegisterPage} />
           </Switch>
         </div>
       </div>
--- src/containers/RegisterPage.js
+++ src/containers/RegisterPage.js
@@ -1,62 +1,76 @@
-import React from 'react'
-import uuidv1 from 'uuid/v1'
-import AxiosInstance from '../config/axiosConfig'
+import React from "react";
+import uuidv1 from "uuid/v1";
+import AxiosInstance from "../config/axiosConfig";
+import "./RegisterPage.css";
 
 class RegisterPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            id: uuidv1(),
-            username: "",
-            password: ""
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: uuidv1(),
+      username: "",
+      password: ""
+    };
+  }
 
-    usernameChange = (event) => {
-        this.setState({
-            username: event.target.value,
-        })
-    }
+  usernameChange = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
 
-    passwordChange = (event) => {
-        this.setState({
-            password: event.target.value,
-        })
-    }
+  passwordChange = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
 
-    onSubmit = () => {
+  onSubmit = () => {
+    const userObject = {
+      id: this.state.id,
+      username: this.state.username,
+      password: this.state.password
+    };
+    AxiosInstance.post("/users/register", userObject)
+      .then(res => {
+        alert("Account Created! Please log in");
+        console.log(res.data);
+      })
+      .catch(error => {
+        alert("Wrong entry");
+        console.log(error);
+      });
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };
 
-        const userObject = {
-            id: this.state.id,
-            username: this.state.username,
-            password: this.state.password
-        }
-        AxiosInstance.post('/users/register', userObject)
-            .then((res) => {
-                alert("Account Created! Please log in")
-                console.log(res.data)            
-            }).catch((error) => {
-                alert("Wrong entry")
-                console.log(error)              
-            })
-        this.setState({
-            username: "",
-            password: ""
-        })
-    }
-
-    render() {
-        return (
-
-            <div>
-                Desired Username:
-            <input type="text" value={this.state.username} onChange={this.usernameChange} /> <br />
-                Desired Password:
-            <input type="password" value={this.state.password} onChange={this.passwordChange} /> <br />
-                <button onClick={this.onSubmit}>Register User</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="login-form">
+        <div className="username">
+          Desired Username:{" "}
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.usernameChange}
+          />
+        </div>
+        <br />
+        <div className="password">
+          Desired Password:{" "}
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.passwordChange}
+          />
+        </div>
+        <br />
+        <button onClick={this.onSubmit}>Register User</button>
+      </div>
+    );
+  }
 }
 
-export default RegisterPage
+export default RegisterPage;
